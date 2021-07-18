8dec696502f4a86e71027d8076a17cb709fbbd32
Adil Sikandar <adil.sikandar@mobilelive.ca>
2021-07-19 01:09:12 +0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,66 @@
+import { Component } from '@angular/core';
+import axios from 'axios';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  strikeCheckout:any = null;
+  stripeKey = 'pk_test_51JCZJNGQvqXDs7J1RxmKDzaMkfcXD1vSYWUVHUcAUMT1OWBmDonpnAtq0BvV5G4dxDoT0vTRWhpciEVPfpKc1W8800XSlzHDpm';
+  checkoutUrl = 'https://checkout.stripe.com/checkout.js';
+  isPaymentDone = false;
+  isSuccess = false;
+  constructor() { }
+
+  ngOnInit() {
+    this.stripePaymentGateway();
+  }
+  
+  checkout(amount: number) {
+    const strikeCheckout = (<any>window).StripeCheckout.configure({
+      key: this.stripeKey,
+      locale: 'auto',
+      token: function (stripeToken: any) {
+        console.log(stripeToken)
+        axios.post('http://localhost:5000/api/v1/users/charge', { stripeToken, amount: amount * 100 }).then((res: any) => {
+          this.isPaymentDone = true;
+          if(res.success === 1) {
+            this.isSuccess = true;
+            }
+            this.isSuccess = false;
+        })
+        // alert('Stripe token generated!');
+      }
+    });
+  
+    strikeCheckout.open({
+      name: 'Offpeak Media',
+      description: 'Offpeak Media One Time Charge',
+      amount: amount * 100
+    });
+  }
+  
+  stripePaymentGateway() {
+    if(!window.document.getElementById('stripe-script')) {
+      const scr = window.document.createElement("script");
+      scr.id = "stripe-script";
+      scr.type = "text/javascript";
+      scr.src = this.checkoutUrl;
+
+      scr.onload = () => {
+        this.strikeCheckout = (<any>window).StripeCheckout.configure({
+          key: this.stripeKey,
+          locale: 'auto',
+          token: function (token: any) {
+            console.log(token)
+    
+          }
+        });
+      }
+        
+      window.document.body.appendChild(scr);
+    }
+  }
+}
